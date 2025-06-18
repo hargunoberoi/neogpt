edfe86984c4ade6e59f45cbf451657de38fa8853
hargun3045 <hargun3045@gmail.com>
2025-06-18 11:02:50 +0530

    Hello, my cold friend

--- inference.py
+++ inference.py
@@ -1,3 +1,4 @@
+#%%
 """
 Simplified inference script for GPT model.
 Loads trained model weights and runs inference on input prompts.
@@ -10,7 +11,7 @@ from model import GPT, GPTConfig
 # Set device and print it
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Using device: {device}")
-
+#%%
 prompt = "Hargun Singh Oberoi is"
 print(f"Prompt: {prompt}")
 
@@ -22,18 +23,21 @@ print("\n[From Scratch] Generating with randomly initialized model...")
 scratch_model = GPT(config).to(device)
 scratch_model.eval()
 # load model weights from models/model.pth
-if os.path.exists("models/model.pth"):
-    model_file = torch.load("models/model.pth", map_location=device,weights_only=True)
-    try:
-        scratch_model.load_state_dict(model_file['model_state_dict'])
-    except Exception as e:
-        print(f"Error loading model state dict: {e}")
-output_text = generate_from_model(prompt, scratch_model, enc, device, max_new_tokens=100)
+# if os.path.exists("models/model.pth"):
+#     model_file = torch.load("models/model.pth", map_location=device,weights_only=True)
+#     try:
+#         scratch_model.load_state_dict(model_file['model_state_dict'])
+#     except Exception as e:
+#         print(f"Error loading model state di  ct: {e}")
+output_text = generate_from_model(prompt, scratch_model, enc, device, max_new_tokens=20)
 print(f"[From Scratch Completion]: {output_text}")
-
+#%%
 # 2. Load pretrained GPT-2 and generate
 print("\n[Pretrained GPT-2] Generating with GPT-2 weights...")
+#%%
 pretrained_model = GPT.from_pretrained("gpt2").to(device)
 pretrained_model.eval()
-output_text = generate_from_model(prompt, pretrained_model, enc, device, max_new_tokens=100)
+output_text = generate_from_model(prompt, pretrained_model, enc, device, max_new_tokens=20)
 print(f"[Pretrained GPT-2 Completion]: {output_text}")
+
+#%%
